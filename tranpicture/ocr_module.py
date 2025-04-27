260a6d87e35cee761e42df7da5c2149d9570b841
guitarkhongem <guitarkhongem@gmail.com>
2025-04-27 10:52:02 +0700

    Update ocr_module.py

--- tranpicture/ocr_module.py
+++ tranpicture/ocr_module.py
@@ -1,8 +1,6 @@
 import easyocr
 
-# Khởi tạo Reader một lần cho đỡ tốn tài nguyên
-reader = easyocr.Reader(['en', 'vi'], gpu=False)
-
 def image_to_text(image_path, lang='en'):
+    reader = easyocr.Reader([lang], gpu=False)
     results = reader.readtext(image_path, detail=0)
     return "\n".join(results)
